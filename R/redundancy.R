0d618b3a4654ea7a14e70926061268780ef29c04
Edwin de Jonge <edwindjonge@gmail.com>
2017-08-02 17:15:20 +0200

    implemented redundancy

--- R/redundancy.R
+++ R/redundancy.R
@@ -1,9 +1,40 @@
 #' @export
 detect_redundancy <- function(x, ...){
   check_validator(x)
-  mip <- errorlocate::miprules(x)
-  mip$objective <- c(x=1)
-  res <- mip$execute()
 }
 
-#x <- validator(x > 1, x  + y < 3, x > 2, y > 0)
+simplify_redundancy <- function(x, ...){
+  check_validator(x)
+  
+  can_be_checked <- errorlocate::is_linear(x) | errorlocate::is_categorical(x) | errorlocate::is_conditional(x)
+  
+  vals <- x$exprs()
+  dnf_set <- lapply(vals[can_be_checked], as_dnf)
+  for (i in rev(seq_along(dnf_set))){  # remove later rules before older rules 
+    if (is_redundant(dnf_set, i)){
+      dnf_set[[i]] <- list()
+    }
+  }
+  vals[can_be_checked] <- lapply(dnf_set, as.expression)
+  vals <- unlist(vals) # this removes empty expressions
+  do.call(validate::validator, vals)
+}
+
+is_redundant <- function(dnf_set, i, ...){
+  dnf <- dnf_set[[i]]
+  negated_rules <- lapply(dnf, invert_or_negate)
+  
+  # Note the single "[" to allow the injection of multiple rules (a negation of a disjunction are multiple rules!)
+  dnf_set[i] <- negated_rules
+  #names(dnf_set) <- make.unique(names(dnf_set))
+  
+  exprs <- unlist(lapply(dnf_set, as.expression))
+  test_rules <- do.call(validate::validator, exprs)
+  is_infeasible(test_rules)
+}
+
+# x <- validator( rule1 = x > 1
+#               , rule2 = x > 2
+#               , rule3 = x > 2
+#               )
+# simplify_redundancy(x)
--- tests/testthat/test-conditional.R
+++ tests/testthat/test-conditional.R
@@ -1,6 +1,6 @@
-context("conditional")
+context("dnf")
 
-test_that("normalize conditional", {
+test_that("dnf", {
   expr <- quote(if (x > 1) y > 3)
   clause <- as_dnf(expr)
   expect_equal(length(clause), 2)
@@ -22,7 +22,37 @@ test_that("clause as if", {
   expr <- quote(if (x > 1) y > 3)
   clause <- as_dnf(expr)
   expect_equal(as.character(clause, as_if = TRUE), "if (x > 1) y > 3")
+  
+  expr <- quote(if (x > 1) (y > 3))
+  clause <- as_dnf(expr)
+  expect_equal(as.character(clause, as_if = TRUE), "if (x > 1) y > 3")
+
+  expr <- quote(!(x > 1)|(y > 3))
+  clause <- as_dnf(expr)
+  expect_equal(as.character(clause, as_if = TRUE), "if (x > 1) y > 3")
+
+  expr <- quote(!(x > 1) | y > 3)
+  clause <- as_dnf(expr)
+  expect_equal(as.character(clause, as_if = TRUE), "if (x > 1) y > 3")
+
+  expr <- quote(!(x > 1 & z > 2) | y > 3)
+  clause <- as_dnf(expr)
+  expect_equal(as.character(clause), "x <= 1 | z <= 2 | y > 3")
+  expect_equal(as.character(clause, as_if=TRUE), "if (x > 1 & z > 2) y > 3")
 })
 
+test_that("simple clause works",{
+  expr <- quote(x > 1)
+  clause <- as_dnf(expr)
+  expect_equal(as.character(clause), "x > 1")
+  
+  expr <- quote((x > 1))
+  clause <- as_dnf(expr)
+  expect_equal(as.character(clause), "x > 1")
+
+  expr <- quote(!(x > 1))
+  clause <- as_dnf(expr)
+  expect_equal(as.character(clause), "x <= 1")
+})
 
 #as_dnf(quote( )
