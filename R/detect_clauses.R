cd3cf79206f5fbb9e1f5c4ed6a77da411cd6390e
Edwin de Jonge <edwindjonge@gmail.com>
2017-08-02 08:00:44 +0200

    wip

--- R/detect_clauses.R
+++ R/detect_clauses.R
@@ -1,23 +1,49 @@
-detect_clauses <- function(x){
+#' @export
+simplify_conditional <- function(x){
   check_validator(x)
   
   is_cond <- errorlocate::is_conditional(x)
   vals <- x$exprs()
-  lapply(vals[is_cond], function(cond){
-    clauses <- as_clause(cond)
-    for (clause in clauses){
-       # if (check_infeasibility(c(vals, clause))){
-       #   
-       # }
-    }
-  })
+  vals[is_cond] <- 
+    lapply(vals[is_cond], function(cond){
+      clauses <- as_clause(cond)
+      clauses <- simplify_non_constraining(clauses, vals)
+      clauses <- simplify_non_restraining(clauses, vals)
+      as.expression(clauses)[[1]]
+    })
+  # TODO set meta data correctly for the resulting rule set
   do.call(validate::validator, vals)
+}
+
+
+simplify_non_restraining <- function(clauses, vals){
+  clauses_s <- lapply(clauses, function(clause){
+    test_rules <- do.call(validate::validator, c(vals, clause))
+    if (is_infeasible(test_rules)){
+      return(NULL)
+    }
+    clause
+  })
+  clauses_s
+}
+
+simplify_non_constraining <- function(clauses, vals){
+  clauses_s <- clauses 
+  for (clause in clauses){
+    clause_neg <- invert_or_negate(clause)
+    test_rules <- do.call(validate::validator, c(vals, clause_neg))
+    if (is_infeasible(test_rules)){
+      clauses_s <- structure(list(clause), "clause")
+      break
+    }
+  }
+  clauses_s
 }
 
 # check_infeais
 # 
-# x <- rules <- validator( if (x > 1) y > 3
+# x <- rules <- validate::validator( if (x > 1) y > 3
 #                        , y < 2
 #                        )
-# 
+# #
 # detect_clauses(rules)
--- tests/testthat/test-is_infeasible.R
+++ tests/testthat/test-is_infeasible.R
@@ -10,8 +10,8 @@ test_that("is_infeasbile works", {
 
 test_that("is_infeasible ignore non mip rules",{
   rules <- validate::validator(x > 1, x < 0, sum(x) > 0)
-  expect_warning(
+  #expect_warning(
     mip <- errorlocate::miprules(rules)
-  )
+  #)
   expect_true(mip$is_infeasible())
 })
