456269a4bc452860d5c273bb7abae7945a6693eb
Edwin de Jonge <edwindjonge@gmail.com>
2025-06-21 14:12:57 +0200

    added test for cat domain check

---
+++ tests/testthat/test-cat_domain.R
@@ -0,0 +1,21 @@
+library(testthat)
+
+context("categorical domain tests")
+
+describe("infeasible", {
+  it("detects infeasible rules with domain set", {
+    v <- validator( a %in% c("a1", "a2"),
+                    a == "a1",
+                    a == "a2"
+                  )
+    expect_true(is_infeasible(v))
+  })
+  
+  it("detects infeasible rules no domain set", {
+    v <- validator( a == "a1",
+                    a == "a2"
+    )
+    expect_true(is_infeasible(v))
+  })
+  
+})
